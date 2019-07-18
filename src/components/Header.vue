325e7fcf39d0169bdc4adedb5b06c810ba2ac819
leotabosa <leeonardotabosa@gmail.com>
2019-07-18 17:34:30 -0300

    up projeto

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-custom">
+            <a class="navbar-brand" href="">Voltar à página inicial</a>
+            <a class="navbar-title" style="color:white; font-size:22px; text-shadow:1px 1px 1px black">Repositórios da Microsoft</a>
+            <a class="navbar-brand" href="https://github.com/Microsoft">GitHub da Microsoft</a>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    nav{
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+    .bg-custom{
+        background-color:black;
+    }
+    .navbar-brand:hover{
+        padding-top:10px;
+        -webkit-transition:0.3s;
+    }
+    .navbar-brand:not(:hover){
+        padding-top:0px;
+        -webkit-transition: 0.3s;
+    }
+    .navbar{
+        position: relative;
+    }
+</style>
+
---
+++ src/components/Loader.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <div class="wrap">
+        <div class="loader"></div>
+    </div>
+</template>
+
+
+<style scoped>
+    .wrap{
+        position:fixed;
+        height:100vh;
+        width:96.9vw;
+        z-index: 1;
+    }
+    .loader{
+        position: fixed;
+        background-image: linear-gradient(#abffd0,green);
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #abffd0;
+        border-right: 16px solid #abffd0;
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spin 2s linear infinite;
+        margin: auto;
+        left: calc(50% - 60px);
+        top: calc(50% - 60px);
+        z-index: 1;
+    }
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+        }
+</style>
